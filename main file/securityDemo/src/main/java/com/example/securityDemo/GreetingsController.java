b2bfe902ee80cdbc40862b8d237f4c6170aa4efd
SwatishaRaj <swatisha.raj@gmail.com>
2025-08-28 23:43:38 -0700

    Added new folder new-feature

---
+++ main file/securityDemo/src/main/java/com/example/securityDemo/GreetingsController.java
@@ -0,0 +1,22 @@
+package com.example.securityDemo;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+@GetMapping("/hello")
+public String sayHello() {
+	return "Hello";
+}
+@PreAuthorize("hasRole('USER')")
+@GetMapping("/user")
+public String userEndpoint() {
+	return "Hello User";
+}
+@GetMapping("/admin")
+public String adminEndpoint() {
+	return "Hello Admin";
+}
+}
---
+++ main file/securityDemo/src/main/java/com/example/securityDemo/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.securityDemo;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		//http.authorizeHttpRequests((requests) -> requests.requestMatchers("/h2-console/**").permitAll().anyRequest().authenticated());
+		System.out.println("SecurityConfig loaded");
+		http
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/h2-console/**").permitAll()  // allow H2 console
+            .anyRequest().authenticated()
+        );
+		http.csrf(csrf->csrf.disable());
+		http.sessionManagement(session ->
+		session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
+		//http.formLogin(withDefaults());
+		http.httpBasic(withDefaults());
+		http.headers(headers -> headers.frameOptions(frameOptions ->frameOptions.sameOrigin()));
+		return http.build();
+	}
+	
+	@Bean
+	public UserDetailsService serDetailsService() {
+		UserDetails user1=User.withUsername("user1").password("{noop}password1").roles("USER").build();
+		UserDetails admin=User.withUsername("admin").password("{noop}password2").roles("ADMIN").build();
+		return new InMemoryUserDetailsManager(user1,admin);
+	}
+
+
+}
